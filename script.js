59691de2925a6a4f9f90076f60cd9b89cd0e95e1
Igor Araújo do Nascimento <igor.araujo.nascimento@escola.pr.gov.br>
2026-03-06 13:50:23 +0000

    site teste

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ let button = document.getElementById('no');
 let height = window.innerHeight - 50;
 let width = window.innerWidth - 50;
 
-button.addEventListener('mouseover', funciton () {
+button.addEventListener('mouseover', function () {
     button.style.position = 'absolute';
     button.style.top = Math.random() * height + "px";
     button.style.left = Math.random() * width + "px";
